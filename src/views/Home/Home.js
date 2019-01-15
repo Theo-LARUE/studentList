8f9829b120aca29ec079cc1c3390d70666a00066
joel2035 <joel.pokam@hetic.net>
2019-01-15 15:57:32 +0100

    init

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import HomeContainer from './HomeContainer';
 
-const Home = () => (
-  <section>
+
+ class Home extends HomeContainer{
+   constructor( props ){
+     super( props );
+     this.state={
+       forName:"",
+       lastName:"",
+       linkGit:"",
+       items:[]
+     }
+    }
+
+  addStudent(event)
+  {
+     event.preventDefault()
+     this.setState({
+     forName: "",
+     lastName: "",
+     linkGit: "",
+     items:[...this.state.items, this.state.forName, this.state.lastName, this.state.linkGit]
+    });
+  }
+  onChange(event)
+  {
+   this.setState(
+     {
+     forName:event.target.value,
+     lastName:event.target.value,
+     linkGit:event.target.value
+   })
+  }
+
+  renderStudent(){
+    return this.state.items.map
+    (
+      (item)=>
+      {
+        return (
+          <div key={item}>
+          {
+            item
+          }
+          </div>
+        )
+      }
+    )
+  }
+
+
+render() {
+  return (
+<section>
     <h1>Student List</h1>
     <h1>Insert your informations</h1>
     <form id="form">
@@ -9,20 +60,38 @@ const Home = () => (
         <label>
           Nom
         </label>
-        <input type="text" placeholder="Lassal" id="firstname" />
+        <input type="text" 
+        value={this.state.forName} 
+        onChange={this.onChange.bind(this)} 
+        placeholder="Lassal" 
+        id="firstname"/>
         <label>
           Prénom
         </label>
-        <input type="text" placeholder="Jean" id="lastname" />
+        <input type="text"
+         value={this.state.LastName}
+          onChange={this.onChange.bind(this)}
+           placeholder="Jean" 
+           id="lastname"/>
         <label>
           Lien Github
         </label>
-        <input type="text" placeholder="Link" id="link" />
-        <input type="submit" value="Send" />
+        <input type="text"
+         value={this.state.linkGit} 
+         onChange={this.onChange.bind(this)} 
+         placeholder="Link" id="link"/>
+        <button 
+        onClick={this.addStudent.bind(this)}
+         type="button">
+         send
+         </button>
       </fieldset>
     </form>
+    <div>{this.renderStudent()}</div>
     </section>
-)
+  );
 
+}
+ }
 
 export default Home;
