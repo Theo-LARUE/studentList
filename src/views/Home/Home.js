6289417f22ea0a4c3511f7607f41d120eb670eaf
Théo Larue <33297696+Theo-LARUE@users.noreply.github.com>
2019-01-18 17:31:20 +0100

    items

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import HomeContainer from './HomeContainer';
 
-
- class Home extends HomeContainer{
-   constructor( props ){
-
-     super( props );
-     this.state={
-       forName:"",
-       lastName:"",
-       linkGit:"",
-       items:[]
-
-     }
-     this.onChange=this.onChange.bind(this)
-     this.deleteUser = this.deleteUser.bind(this)
+  class Home extends HomeContainer{
+    constructor( props ){
+      super( props );
+      this.state={
+        forName:"",
+        lastName:"",
+        linkGit:"",
+        items:[]
+      }
+      this.onChange=this.onChange.bind(this)
+      this.deleteUser = this.deleteUser.bind(this)
     }
 
   addStudent(event)
   {
-     event.preventDefault()
-
-     const { forName, lastName, linkGit } = this.state
-
-     this.setState({
+    event.preventDefault()
+    const { forName, lastName, linkGit } = this.state
+    this.setState({
       forName: "",
       lastName: "",
       linkGit: "",
@@ -39,20 +34,20 @@ import HomeContainer from './HomeContainer';
   }
   onChange(event, name)
   {
- 
-   this.setState(
-     {
+
+    this.setState(
+      {
     [name]:event.target.value
-   })
+    })
   }
   deleteUser(event){
-   event.preventDefault();
-   const array = this.state.items;
-   const index = array.indexOf(event.target.value);
-   array.splice(index, 1);
-   this.setState({
-     items:array
-   })
+    event.preventDefault();
+    const array = this.state.items;
+    const index = array.indexOf(event.target.value);
+    array.splice(index, 1);
+    this.setState({
+      items:array
+    })
 
   }
 
@@ -64,7 +59,7 @@ import HomeContainer from './HomeContainer';
         return (
           <div key={item}>
             <li>
-              <span>{item.forName}</span> - <span>{item.lastName}</span> - <a href="#" target="_blank">{item.linkGit} </a> 
+              <span>{item.forName}  </span> - <span>{item.lastName}</span> - <a href="#" target="_blank">{item.linkGit} </a> 
               <button onClick = { this.deleteUser.bind(this) } >X</button>
             </li>
           </div>
@@ -80,38 +75,13 @@ render() {
     <form id="form">
       <fieldset>
         <legend>Insert your informations</legend> 
-        <label>
-          Nom
-        </label>
-        <input type="text" 
-        value={this.state.forName} 
-        onChange={(event) => this.onChange(event, 'forName')} 
-        placeholder="Lassal" 
-        id="firstname"/>
-        <label>
-          Prénom
-        </label>
-        <input type="text"
-         value={this.state.lastName}
-          onChange = {
-            (event) => this.onChange(event, 'lastName')
-          }
-           placeholder="Jean" 
-           id="lastname"/>
-        <label>
-          Lien Github
-        </label>
-        <input type="text"
-         value={this.state.linkGit} 
-         onChange = {
-           (event) => this.onChange(event, 'linkGit')
-         }
-         placeholder="Link" id="link"/>
-        <button 
-        onClick={this.addStudent.bind(this)}
-         type="button">
-         send
-         </button>
+        <label>Nom</label>
+        <input type="text" value={this.state.forName} onChange={(event) => this.onChange(event, 'forName')} placeholder="Lassal" id="firstname"/>
+        <label>Prénom</label>
+        <input type="text" value={this.state.lastName} onChange = {(event) => this.onChange(event, 'lastName')} placeholder="Jean" id="lastname"/>
+        <label>Lien Github</label>
+        <input type="text" value={this.state.linkGit} onChange = {(event) => this.onChange(event, 'linkGit')} placeholder="Link" id="link"/>
+        <button onClick={this.addStudent.bind(this)} type="button"> send </button>
       </fieldset>
     </form>
     <div>{this.renderStudent()}</div>
